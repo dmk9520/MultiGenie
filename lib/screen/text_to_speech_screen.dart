b2a7f8a6fe4095c44d82873f0eaae822cdef10a4
Dhruvit Katrodiya <dmk9520@icloud.com>
2023-12-07 16:19:07 +1030

    Speech to Text Updated

--- lib/screen/text_to_speech_screen.dart
+++ lib/screen/text_to_speech_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class TTS extends StatefulWidget {
   const TTS({Key? key}) : super(key: key);
@@ -11,6 +14,71 @@ class TTS extends StatefulWidget {
 class _TTSState extends State<TTS> {
   FlutterTts flutterTts = FlutterTts();
   TextEditingController controller = TextEditingController();
+  final TextEditingController _textController = TextEditingController();
+
+  final SpeechToText _speechToText = SpeechToText();
+  bool _speechEnabled = false;
+  String _lastWords = "";
+
+  void listenForPermissions() async {
+    final status = await Permission.microphone.status;
+    switch (status) {
+      case PermissionStatus.denied:
+        requestForPermission();
+        break;
+      case PermissionStatus.granted:
+        break;
+      case PermissionStatus.limited:
+        break;
+      case PermissionStatus.permanentlyDenied:
+        break;
+      case PermissionStatus.restricted:
+        break;
+      case PermissionStatus.provisional:
+        // TODO: Handle this case.
+        break;
+    }
+  }
+  Future<void> requestForPermission() async {
+    await Permission.microphone.request();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    listenForPermissions();
+    if (!_speechEnabled) {
+      _initSpeech();
+    }
+  }
+
+  void _initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+  }
+
+  void _startListening() async {
+    await _speechToText.listen(
+      onResult: _onSpeechResult,
+      listenFor: const Duration(seconds: 30),
+      localeId: "en_En",
+      cancelOnError: false,
+      partialResults: false,
+      listenMode: ListenMode.confirmation,
+    );
+    setState(() {});
+  }
+
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState(() {});
+  }
+
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      _lastWords = "$_lastWords${result.recognizedWords} ";
+      _textController.text = _lastWords;
+    });
+  }
 
   double volume = 1.0;
   double pitch = 1.0;
@@ -26,9 +94,10 @@ class _TTSState extends State<TTS> {
           title: const Text("Text To Speech"),
         ),
         body: Container(
-          margin: const EdgeInsets.only(top: 20),
+          margin: const EdgeInsets.all(10),
           child: Center(
-            child: Column(children: [
+            child: Column(
+                children: [
               Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
@@ -59,6 +128,40 @@ class _TTSState extends State<TTS> {
                   ),
                 ],
               ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: _textController,
+                          minLines: 6,
+                          maxLines: 10,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey.shade300,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      FloatingActionButton.small(
+                        onPressed:
+                        // If not yet listening for speech start, otherwise stop
+                        _speechToText.isNotListening
+                            ? _startListening
+                            : _stopListening,
+                        tooltip: 'Listen',
+                        backgroundColor: Colors.blueGrey,
+                        child: Icon(_speechToText
+                            .isNotListening
+                            ? Icons.mic_off
+                            : Icons.mic),
+                      )
+                    ],
+                  ),
               ]),),),);
   }
 
